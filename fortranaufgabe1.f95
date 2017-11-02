b8a2662e3f54b4c2d790b7202af598068cabcd44
JeannineBorn <jeannineborn@aol.com>
2017-11-02 15:40:29 +0100

    Added for-loop from 100 to 2000

--- fortranaufgabe1.f95
+++ fortranaufgabe1.f95
@@ -15,41 +15,42 @@ program multiplicate
    	integer ( kind = 4 ) mm, mm2, m, m2, s, s2
 	integer values(8), values2(8)
 
-
-    read (*,*) n
-    write (*,*) n
-	allocate(matrix1(n,n))
-    allocate(matrix2(n,n))
-    allocate(matrix3(n,n))
+	do n = 100, 2000, 100
+		allocate(matrix1(n,n))
+    	allocate(matrix2(n,n))
+    	allocate(matrix3(n,n))
     
-	do i = 1, n
-    	do j= 1, n
-        	matrix1(i,j) = 0.02*j
-            matrix2(i,j) = 0.01*j
-            matrix3(i,j) = 0
-        end do
-    end do
+		do i = 1, n
+    		do j= 1, n
+        		matrix1(i,j) = 0.02*j
+            	matrix2(i,j) = 0.01*j
+            	matrix3(i,j) = 0
+        	end do
+    	end do
     
-	call date_and_time(values = values)
-	do i = 1, n
-    	do j = 1, n
-        	do k = 1, n
-            	matrix3(i,j) = matrix1(i,k) * matrix2(k,j) + matrix3(i,j)
-            end do
-        end do
-    end do
-    call date_and_time(values = values2)
+		call date_and_time(values = values)
+		do i = 1, n
+    		do j = 1, n
+        		do k = 1, n
+            		matrix3(i,j) = matrix1(i,k) * matrix2(k,j) + matrix3(i,j)
+            	end do
+        	end do
+    	end do
+    	call date_and_time(values = values2)
+		m = values(6)
+  		s = values(7)
+  		mm = values(8)
 
-  	m = values(6)
-  	s = values(7)
-  	mm = values(8)
-
-  	m2 = values2(6)
-  	s2 = values2(7)
-  	mm2 = values2(8)
+	  	m2 = values2(6)
+  		s2 = values2(7)
+  		mm2 = values2(8)
 	
-    write (*,*) 'diff minutes', m2 - m
-    write (*,*) 'diff seconds', s2 - s
-    write (*,*) 'diff milliseconds', mm2 - mm
-    !call showMatrix(matrix3, n)
+    	write (*,*) 'diff minutes', m2 - m
+    	write (*,*) 'diff seconds', s2 - s
+    	write (*,*) 'diff milliseconds', mm2 - mm
+    	deallocate(matrix1)
+        deallocate(matrix2)
+        deallocate(matrix3)
+		!call showMatrix(matrix3, n)
+	end do
 end program multiplicate
